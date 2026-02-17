d5e130974f60e0034761c72ded9996921958cf36
Demian Kozii <ivan.ivanovych10@gmail.com>
2026-02-17 19:55:15 +0200

    dqw

--- algo/Homework/HM1/HM1/Source.cpp
+++ algo/Homework/HM1/HM1/Source.cpp
@@ -6,7 +6,7 @@ int main() {
 	std::chrono::nanoseconds ser1(0), ser2(0), ser3(0);
 
 	auto start = std::chrono::high_resolution_clock::now();
-	for (int i = 0; i < 100000; i++) {
+	for (int i = 0; i < 1000000; i++) {
 		a = (rand() % 100) * pow(-1, rand() % 2);
 		b = (rand() % 100) * pow(-1, rand() % 2);
 		c = (rand() % 100) * pow(-1, rand() % 2);
@@ -17,7 +17,7 @@ int main() {
 	ser1 = chrono::duration_cast<chrono::nanoseconds>(end - start);
 
 	start = std::chrono::high_resolution_clock::now();
-	for (int i = 0; i < 100000; i++) {
+	for (int i = 0; i < 1000000; i++) {
 		a = (rand() % 100) * pow(-1, rand() % 2);
 		b = (rand() % 100) * pow(-1, rand() % 2);
 		c = (rand() % 100) * pow(-1, rand() % 2);
@@ -28,7 +28,7 @@ int main() {
 	ser2 = chrono::duration_cast<chrono::nanoseconds>(end - start);
 
 	start = std::chrono::high_resolution_clock::now();
-	for (int i = 0; i < 100000; i++) {
+	for (int i = 0; i < 1000000; i++) {
 		a = (rand() % 100) * pow(-1, rand() % 2);
 		b = (rand() % 100) * pow(-1, rand() % 2);
 		c = (rand() % 100) * pow(-1, rand() % 2);
@@ -39,9 +39,9 @@ int main() {
 	ser3 = chrono::duration_cast<chrono::nanoseconds>(end - start);
 
 	
-	ser1 /= 100000;
-	ser2 /= 100000;
-	ser3 /= 100000;
+	ser1 /= 1000000;
+	ser2 /= 1000000;
+	ser3 /= 1000000;
 	
 	cout << "Method 1: " << ser1.count() << " Method 2: " << ser2.count() << " Method 3: " << ser3.count() << endl;
 
